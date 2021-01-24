ac2360ecee8bad796b8204e00f911c51439a17a1
vishal <Vk939619@gmail.com>
2021-01-24 22:27:40 +0530

    add stock and delivery boys added

---
+++ js/excel.js
@@ -0,0 +1,26 @@
+function fnExcelReport() {
+    var table = document.getElementById('dataTable'); // id of table
+    var tableHTML = table.outerHTML;
+    var fileName = 'download.xls';
+
+    var msie = window.navigator.userAgent.indexOf("MSIE ");
+
+    // If Internet Explorer
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        dummyFrame.document.open('txt/html', 'replace');
+        dummyFrame.document.write(tableHTML);
+        dummyFrame.document.close();
+        dummyFrame.focus();
+        return dummyFrame.document.execCommand('SaveAs', true, fileName);
+    }
+    //other browsers
+    else {
+        var a = document.createElement('a');
+        tableHTML = tableHTML.replace(/  /g, '').replace(/ /g, '%20'); // replaces spaces
+        a.href = 'data:application/vnd.ms-excel,' + tableHTML;
+        a.setAttribute('download', fileName);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+}
